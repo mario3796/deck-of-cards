dfacacf915f3ee89e7409e72ac2706c3c79a296e
Mario Haleem <mariohaleem@gmail.com>
2022-03-31 20:32:18 +0200

    edit app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import CardDetails from './components/Cards/Card/CardDetails/CardDetails';
+
+import Feed from './components/Feed/Feed';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" exact element={<Feed />} />
+    <Route exact
+    path='/card-details'
+    element={<CardDetails />} />
+  </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
